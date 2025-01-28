5963db72b91a9120718730ee08f032598b535e03
Stoic-123 <longbigboss123@gmail.com>
2025-01-28 15:29:49 +0700

    long

--- qrTrackAttendance/routes/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { signUpPost, loginPost, logout } = require("../controllers/auth");
-router.post("/auth/signup", signUpPost);
-router.post("/auth/login", loginPost);
-router.delete("/auth/logout", logout);
-module.exports = router;
--- qrTrackAttendance/routes/qr.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const qrController = require("../controllers/qr");
-
-router.get("/qr/generate", qrController.generateQR);
-router.post("/qr/scan", qrController.scanQR);
-router.get("/qr/latest", qrController.getLatestQR);
-module.exports = router;
